d9322e774b1c0cc9a21f6476a852401b675b7b7a
Dov Azogui <dov.azogui@gmail.com>
2024-08-25 21:37:20 -0400

    created variables

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 40
+let num4 = 12
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4
+const max = Math.max(1, 2, 3, 4, 5, 20);
